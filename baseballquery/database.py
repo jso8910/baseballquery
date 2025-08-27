1b73cfcede16ad0832873526c256ae660847e21d
Jason R <mail4jasonr@gmail.com>
2025-08-26 17:22:07 -0700

    Fixed error when no database file present

--- baseballquery/database.py
+++ baseballquery/database.py
@@ -7,4 +7,6 @@ if not data_dir.exists():
     data_dir.mkdir()
 # Create a SQLite database in the data directory
 db_path = data_dir / "baseballquery.db"
+if not db_path.exists():
+    db_path.touch()
 engine = sqlalchemy.create_engine(f"sqlite:///{db_path}")
